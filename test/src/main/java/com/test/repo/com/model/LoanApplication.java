b18f8b44a543d073102ed1b867bf39270d4253a4
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2024-02-25 16:17:37 +0530

    Updated

--- test/src/main/java/com/test/repo/com/model/LoanApplication.java
+++ test/src/main/java/com/test/repo/com/model/LoanApplication.java
@@ -1,49 +1,42 @@
-Here is the Entity/Model class with the package name `com.test.repo.com.model` for the given repository:
+Apologies for the confusion. Here is an example of a LoanApplication entity/model class in the com.test.repo.com.model package:
 
 ```java
 package com.test.repo.com.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "loan_applications")
 public class LoanApplication {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(name = "loan_id")
     private String loanId;
-    
-    // Add more fields as per your requirements
-    
-    public LoanApplication() {
-        // Default constructor
-    }
-    
-    // Add getters and setters for all the fields
-    
+
+    // Add any additional fields and methods required for your application
+
     public Long getId() {
         return id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     public String getLoanId() {
         return loanId;
     }
-    
+
     public void setLoanId(String loanId) {
         this.loanId = loanId;
     }
-    
-    // Add more getters and setters for the additional fields
-    
+
+    // Add getters and setters for additional fields
+
 }
 ```
 
-Make sure to replace `com.test.repo.com.model` with the actual package name for your model class. This entity/model class represents a loan application with fields such as `id` and `loanId`. You can add more fields as per your requirements and also include their respective getters and setters.
+Make sure to adjust the fields and annotations according to your application requirements.

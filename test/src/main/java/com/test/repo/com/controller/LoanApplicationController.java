b18c25e436f308aa4c0881ca1f85bd155c3301af
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2024-02-25 16:18:20 +0530

    Updated

--- test/src/main/java/com/test/repo/com/controller/LoanApplicationController.java
+++ test/src/main/java/com/test/repo/com/controller/LoanApplicationController.java
@@ -1,4 +1,4 @@
-Sure! Here is a Spring Boot Controller class for the given service classes:
+Sure! Here is an example of a Spring Boot controller class that uses the provided service classes and package name "com.test.repo.com.controller":
 
 ```java
 package com.test.repo.com.controller;
@@ -6,10 +6,9 @@ package com.test.repo.com.controller;
 import com.test.repo.com.model.LoanApplication;
 import com.test.repo.com.service.LoanApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/loan-applications")
@@ -23,21 +22,22 @@ public class LoanApplicationController {
     }
 
     @GetMapping("/{loanId}")
-    public LoanApplication getLoanApplication(@PathVariable String loanId) {
-        return loanApplicationService.findByLoanId(loanId);
+    public ResponseEntity<LoanApplication> getLoanApplication(@PathVariable String loanId) {
+        LoanApplication loanApplication = loanApplicationService.findByLoanId(loanId);
+        if (loanApplication != null) {
+            return ResponseEntity.ok(loanApplication);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    // Add more controller methods for other operations if needed
+    // Add other controller methods as needed
 
 }
 ```
 
-In this example, the `LoanApplicationController` class is annotated with `@RestController` to indicate that it is a Spring MVC Controller that handles HTTP requests and returns JSON responses.
+In this example, the `LoanApplicationController` class is annotated with `@RestController` to indicate that it is a RESTful controller. We use the `LoanApplicationService` to retrieve the loan application data and return it as a response entity.
 
-The `LoanApplicationController` class has a constructor that takes an instance of `LoanApplicationService` as a parameter and uses constructor-based dependency injection to inject the service bean.
+The `@GetMapping` annotation is used to map the GET request to the "/loan-applications/{loanId}" endpoint, where {loanId} is a path variable. The `getLoanApplication` method retrieves the loan application by calling the `findByLoanId` method of the service class and returns a response entity.
 
-The `getLoanApplication` method is mapped to the `/loan-applications/{loanId}` endpoint using the `@GetMapping` annotation. It retrieves the loan application details based on the loan ID by calling the `findByLoanId` method of the `LoanApplicationService` and returns the result as a JSON response.
-
-You can add more controller methods to this class for other operations such as creating, updating, or deleting loan applications, based on your specific requirements.
-
-Please make sure the package name and imports are adjusted according to your project structure.
+You can add additional controller methods to handle other API endpoints as needed. Remember to adjust the package name and imports according to your project's structure.
